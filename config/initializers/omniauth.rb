e818d2a19ff017fbeca236b84d2d4c4f21349991
amardaxini <amardaxini@gmail.com>
2011-02-24 17:12:27 +0530

    remove key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'l68x1wWPmMhidGDoJp45Q', 'PJMo9N550TX7UxBb9HKZshhWIMvWRmMQjH80GcBIRoo'
+  provider :twitter, '', ''
 end
