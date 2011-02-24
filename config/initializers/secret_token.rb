4522efbc41306ab77216f8fe6a1b8cfac4934b72
amardaxini <amardaxini@gmail.com>
2011-02-24 16:45:27 +0530

    initial commit added authentcation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rug::Application.config.secret_token = 'f6214c2bffb3e8719281c9c2aef10c9ceca34945912ac82b9c5d85e8e288511e324bba10e027eaab9d1f0490e8f14bd09d53a995d753e5bbb2beddfdd2acd0c2'
---
+++ public/javascripts/application.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+//     $("li.main-li").mouseover(function(){ //When mouse over ...
+//            console.log($(this).find('.subnav-ul'));
+//           //Following event is applied to the subnav itself (making height of subnav 60px)
+//          $(this).find('.subnav-ul').stop().animate({height: '60px', opacity:'2'}, 1500);
+//    });
+//
+//    $("ul.main-ul li.main-li").mouseout(function(){ //When mouse out ...
+//          //Following event is applied to the subnav itself (making height of subnav 0px)
+//          $(this).find('.subnav-ul').stop().animate({height:'0px', opacity:'0'},{queue:false, duration:1600, easing: 'linear'})
+//    });
+//       //menu item background color animation
+//    $("li").hover(function() {
+//             $(this).stop().animate({ backgroundColor: "#C13D93"}, 600);},
+//          function() {
+//             $(this).stop().animate({ backgroundColor: "#333333" }, 600);
+//       });
+});
