a1aff3e7ad6069b3f20955109286d5cbcb50b5e1
amardaxini <amardaxini@gmail.com>
2011-03-10 13:40:38 +0530

    change omniauth provider key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :github, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 
 end
