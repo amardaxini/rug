7e10361d4f8ec3400443f368376c43cea3914f1a
amardaxini <amardaxini@gmail.com>
2011-03-10 14:20:34 +0530

    fixed bug in creating authentications

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < Devise::SessionsController
 	def create
     super
     unless session[:omniauth].nil?
-	    current_user.authentications.create!(:provider => session[:omniauth]['provider'], :uid => session[:omniauth]['uid'])
+	    current_user.authentications.create!(:provider => session[:omniauth]['provider'], :uid => session[:omniauth]['uid'].to_i)
 	    session[:omniauth] = nil
     end
 
